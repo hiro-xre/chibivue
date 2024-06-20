5b0203de83fd5ce3429275a092d32fd17e0e95b4
hiroto-hara <xre0605@gmail.com>
2024-06-20 21:52:07 +0900

    vdom完了

--- packages/runtime-core/renderer.ts
+++ packages/runtime-core/renderer.ts
@@ -3,9 +3,10 @@ import { ReactiveEffect } from '../reactivity'
 import { Component } from './component'
 
 export interface RendererOptions<HostNode = RendererNode, HostElement = RendererElement> {
-  patchProp(el: HostElement, key: string, value: any): void;
+  patchProp(el: HostElement, key: string, value: any): void
   createElement(type: string): HostNode
   createText(text: string): HostNode
+  setText(node: HostNode, text: string): void
   setElementText(node: HostNode, text: string): void
   insert(child: HostNode, parent: HostNode, anchor?: HostNode | null): void
 }
@@ -27,6 +28,7 @@ export function createRenderer(options: RendererOptions) {
     patchProp: hostPatchProp,
     createElement: hostCreateElement,
     createText: hostCreateText,
+    setText: hostSetText,
     insert: hostInsert,
   } = options
 
@@ -47,7 +49,7 @@ export function createRenderer(options: RendererOptions) {
     if (n1 === null) {
       mountElement(n2, container)
     } else {
-      // patchElement(n1, n2);
+      patchElement(n1, n2);
     }
   }
 
@@ -70,11 +72,31 @@ export function createRenderer(options: RendererOptions) {
   const mountChildren = (children: VNode[], container: RendererElement) => {
     for (let i = 0; i < children.length; i++) {
       const child = (children[i] = normalizeVNode(children[i]))
-
-      // TODO: このログの挙動を理解する
-      console.log('children', children)
-
       patch(null, child, container)
+    }
+  }
+
+  const patchElement = (n1: VNode, n2: VNode) => {
+    const el = (n2.el = n1.el!)
+
+    const props = n2.props
+
+    patchChildren(n1, n2, el)
+
+    for (const key in props) {
+      if (props[key] !== n1.props?.[key] ?? {}) {
+        hostPatchProp(el, key, props[key])
+      }
+    }
+  }
+
+  const patchChildren = (n1: VNode, n2: VNode, container: RendererElement) => {
+    const c1 = n1.children as VNode[]
+    const c2 = n2.children as VNode[]
+
+    for (let i = 0; i < c2.length; i++) {
+      const child = (c2[i] = normalizeVNode(c2[i]))
+      patch(c1[i], child, container)
     }
   }
 
@@ -86,7 +108,10 @@ export function createRenderer(options: RendererOptions) {
     if (n1 == null) {
       hostInsert((n2.el = hostCreateText(n2.children as string)), container)
     } else {
-      // TODO: patch
+      const el = (n2.el = n1.el!)
+      if (n2.children !== n1.children) {
+        hostSetText(el, n2.children as string)
+      }
     }
   }
 
